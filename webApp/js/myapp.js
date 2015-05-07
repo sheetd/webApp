5c8a0a44a36c9c1af21b971cb4328350d8ba7f46
Neil Meredith <neil@sheetd.com>
2015-05-07 12:02:34 -0400

    Tokens

--- webApp/js/myapp.js
+++ webApp/js/myapp.js
@@ -1,9 +1,9 @@
 function initialize() {
   var options = {
-    'document' : 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bm1fYnVja2V0L01HTV9EU1RfUGFuZWxMLkNBVFBhcnQ=',
-    'env':'AutodeskProduction',
-    'getAccessToken': getToken,
-    'refreshToken': getToken,
+    "document" : "urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bm1fYnVja2V0L01HTV9EU1RfUGFuZWxMLkNBVFBhcnQ=",
+    "env" : "AutodeskProduction",
+    "getAccessToken" : getToken,
+    "refreshToken" : getToken
   };
   var viewerElement = document.getElementById('viewer');
   var viewer = new Autodesk.Viewing.Viewer3D(viewerElement, {});
@@ -21,7 +21,7 @@ function getToken() {
   });
   return accessToken;
   */
-  return "AaBUsx35j2MY6rtAmyiXlvX84nqp";
+  return "duxEqWiLPFnJLdUbL0RbACWxY0NU";
 }  
 
 
@@ -30,8 +30,8 @@ function loadDocument(viewer, documentId) {
   Autodesk.Viewing.Document.load(documentId, function(doc) {// onLoadCallback
   var geometryItems = [];
   geometryItems = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {
-      type : "geometry",
-      role : "3d"
+      "type" : "geometry",
+      "role" : "3d"
   }, true);
   if (geometryItems.length > 0) {
       viewer.load(doc.getViewablePath(geometryItems[0]));
